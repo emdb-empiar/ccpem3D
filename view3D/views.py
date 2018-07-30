d94fa29ed5e7d899cb6bee7c1486a179b72d54be
Sriram <sriram@ebi.ac.uk>
2018-07-30 14:53:51 +0100

    pdb model renderer (with spheres and sticks) added to 3d viewer

--- view3D/views.py
+++ view3D/views.py
@@ -7,6 +7,7 @@ import os
 
 class FileForm(forms.Form):
     vtp_file = forms.FileField(label="VTP File:")
+    pdb_file = forms.FileField(label="PDB File:", required=False)
 
 
 def handle_uploaded_file(f):
@@ -26,4 +27,12 @@ class Index(FormView):
         context = self.get_context_data()
         context['form'] = form
         context['image_file'] = image_file.name
+        if form.cleaned_data['pdb_file']:
+            model_file = form.cleaned_data['pdb_file']
+            handle_uploaded_file(model_file)
+            context['model_file'] = model_file.name
+            if model_file.name.endswith('ent'):
+                context['model_id'] = model_file.name.split('.ent')[0]
+            elif model_file.name.endswith('pdb'):
+                context['model_id'] = model_file.name.split('.pdb')[0]
         return render(self.request, 'view3D/index.html', context)
